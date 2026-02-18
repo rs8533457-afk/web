5c78d51eff9d60d9d8a141e57abd6d09ad880f24
rs8533457-afk <rs8533457@example.com>
2026-02-18 18:19:28 +0530

    Final cleanup of SQL and RLS policies

--- supabase_setup.sql
+++ supabase_setup.sql
@@ -103,28 +103,3 @@ CREATE POLICY "storage_owner_policy"
 ON storage.objects FOR ALL
 USING ( bucket_id = 'files' AND (storage.foldername(name))[1] = auth.uid()::text )
 WITH CHECK ( bucket_id = 'files' AND (storage.foldername(name))[1] = auth.uid()::text );
-
--- Storage Policies for 'files' bucket
--- Allow users to upload files to their own folder
-CREATE POLICY "Users can upload to their own folder"
-ON storage.objects FOR INSERT
-WITH CHECK (
-  bucket_id = 'files' AND
-  (storage.foldername(name))[1] = auth.uid()::text
-);
-
--- Allow users to view their own files
-CREATE POLICY "Users can view their own files"
-ON storage.objects FOR SELECT
-USING (
-  bucket_id = 'files' AND
-  (storage.foldername(name))[1] = auth.uid()::text
-);
-
--- Allow users to delete their own files
-CREATE POLICY "Users can delete their own files"
-ON storage.objects FOR DELETE
-USING (
-  bucket_id = 'files' AND
-  (storage.foldername(name))[1] = auth.uid()::text
-);
